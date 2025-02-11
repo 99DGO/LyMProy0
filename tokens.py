ef98f8328ab79b11677c432f75e422cd4da5a24e
99DGO <d.gonzalezo2@uniandes.edu.co>
2025-02-10 20:31:14 -0500

    tokens declarados

---
+++ tokens.py
@@ -0,0 +1,74 @@
+
+#Constantes tokens
+TK_VAR_DIV="|"
+TK_VAR_NAME
+TK_VAR_ASSIGN=":="
+
+TK_CODEBLOCK_DIVLEFT="["
+TK_CODEBLOCK_DIVRIGHT="]"
+
+TK_PROC= "proc"
+TK_PROC_NAME
+TK_PARAM_NAME
+
+#Tokens instrucciones
+TK_GOTO="goto:"
+TK_WITH="with:"
+
+TK_MOVE="move:"
+
+TK_TURN="turn:"
+#tokens O
+TK_LEFT="#left"
+TK_RIGHT="#right"
+TK_AROUND="#around"
+
+TK_FACE="face:"
+#tokens D
+TK_NORTH="#north"
+TK_SOUTH="#south"
+TK_WEST="#west"
+TK_EAST="#east"
+
+TK_PUT="put"
+TK_BALLOONS="#balloons"
+TK_CHIPS="#chips"
+TK_PICK="pick:"
+
+TK_MOVE="move:"
+TK_TOTHE="toThe:"
+TK_INDIR="inDir:"
+
+TK_JUMP="jump:"
+
+TK_NOP="nop"
+
+#Tokens condicionales y loops
+TK_IF="if:"
+TK_THEN="then:"
+TK_ELSE="else:"
+
+TK_WHILE="while:"
+TK_DO="do:"
+
+TK_FOR="for:"
+TK_REPEAT="repeat:"
+
+#Tokens condiciones
+TK_FACING="facing:"
+
+TK_CANPUT="canput:"
+TK_OFTYPE="ofType:"
+TK_CANPICK="canPick:"
+
+TK_CANMOVE="canMove:"
+TK_CANJUMP="canJump:"
+
+TK_NOT="not:"
+
+TK_PUNTO="."
+TK_NUMERO
+#Usado cuando llamas procedure, asignar variable
+
+
+
