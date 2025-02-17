c9160f231ccac274e0bdd326f13d3ef4fa4e0ae2
Sofia Arias <s.ariasz2@uniandes.edu.co>
2025-02-17 17:54:20 -0500

    test instructions2

--- mainTester.py
+++ mainTester.py
@@ -2,9 +2,9 @@ import lexer as lx
 import parser as ps
 
 carpeta="test_texts/"
-lstArchivos = ["code-examples.txt", "TrueInstructions.txt"]
-    #, "FalseMove2.txt", "TrueInstructions.txt", "FalseGoTo.txt","FalseGoTo2.txt", "FalseGoTo3.txt", "FalseFace.txt", "FalseMove.txt", "FalseMove2.txt", "FalsePut.txt", "FalsePut2.txt", "FalsePick.txt", "FalseMoveToThe.txt", "FalseMoveInDir.txt",
-     #          "FalseMoveInDir2.txt", "FalseNop.txt", "FalseVarDiv.txt", "FalseVarDiv2.txt", "FalseJumpInDir.txt", "FalseJumpToThe.txt"] 
+lstArchivos = ["TrueInstructions.txt"]
+#, "FalseGoTo.txt","FalseGoTo2.txt", "FalseGoTo3.txt", "FalseFace.txt", "FalseMove.txt", "FalseMove2.txt", "FalsePut.txt", "FalsePut2.txt", "FalsePick.txt", "FalseMoveToThe.txt", "FalseMoveInDir.txt",
+#               "FalseMoveInDir2.txt", "FalseNop.txt", "FalseVarDiv.txt", "FalseVarDiv2.txt", "FalseJumpInDir.txt", "FalseJumpToThe.txt"] 
 
 for archName in lstArchivos:
     codigo = ""
