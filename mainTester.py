377b3a4adbbefbb6cf9ae17963dd8907385fe44b
99DGO <d.gonzalezo2@uniandes.edu.co>
2025-02-17 17:09:05 -0500

    fix sublist token y main codeblock div left

--- mainTester.py
+++ mainTester.py
@@ -2,8 +2,9 @@ import lexer as lx
 import parser as ps
 
 carpeta="test_texts/"
-lstArchivos = ["TrueInstructions.txt", "FalseGoTo.txt","FalseGoTo2.txt", "FalseGoTo3.txt", "FalseFace.txt", "FalseMove.txt", "FalseMove2.txt", "FalsePut.txt", "FalsePut2.txt", "FalsePick.txt", "FalseMoveToThe.txt", "FalseMoveInDir.txt",
-               "FalseMoveInDir2.txt", "FalseNop.txt", "FalseVarDiv.txt", "FalseVarDiv2.txt", "FalseJumpInDir.txt", "FalseJumpToThe.txt"] 
+lstArchivos = ["TrueInstructions.txt"]
+#, "FalseGoTo.txt","FalseGoTo2.txt", "FalseGoTo3.txt", "FalseFace.txt", "FalseMove.txt", "FalseMove2.txt", "FalsePut.txt", "FalsePut2.txt", "FalsePick.txt", "FalseMoveToThe.txt", "FalseMoveInDir.txt",
+#               "FalseMoveInDir2.txt", "FalseNop.txt", "FalseVarDiv.txt", "FalseVarDiv2.txt", "FalseJumpInDir.txt", "FalseJumpToThe.txt"] 
 
 for archName in lstArchivos:
     codigo = ""
